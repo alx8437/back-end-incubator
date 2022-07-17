173f73355cf77edf810ff36b8cb9b0ee43345d1c
aleksandr_zelenskiy <dezion777@gmail.com>
2022-07-17 14:59:01 +0400

    create prettier

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,47 @@
-import express from 'express';
-import cors from 'cors';
+import express, { Response, Request } from "express";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
 
 const port = process.env.PORT || 5000;
 
-const videos = [
-    {id: 1, title: 'About JS - 01', author: 'it-incubator.eu'},
-    {id: 2, title: 'About JS - 02', author: 'it-incubator.eu'},
-    {id: 3, title: 'About JS - 03', author: 'it-incubator.eu'},
-    {id: 4, title: 'About JS - 04', author: 'it-incubator.eu'},
-    {id: 5, title: 'About JS - 05', author: 'it-incubator.eu'},
-]
+// const videos = [
+//     {id: 1, title: 'About JS - 01', author: 'it-incubator.eu'},
+//     {id: 2, title: 'About JS - 02', author: 'it-incubator.eu'},
+//     {id: 3, title: 'About JS - 03', author: 'it-incubator.eu'},
+//     {id: 4, title: 'About JS - 04', author: 'it-incubator.eu'},
+//     {id: 5, title: 'About JS - 05', author: 'it-incubator.eu'},
+// ]
 
-app.get('/', (req: any, res: any) => {
-    let helloWorld = 'Go study123123123';
-    res.send(helloWorld)
-})
+const products = [{ title: "cheese" }, { title: "mushrooms" }];
+const addresses = [{ title: "WallStreet" }, { title: "Gagarin" }];
 
-app.get('/videos', (req: any, res: any) => {
-    res.status(200);
-    res.send(videos)
-})
+app.get("/", (req: any, res: any) => {
+  let helloWorld = "Go study123123123";
+  res.send(helloWorld);
+});
 
-app.get('/videos/:videoId', (req: any, res: any) => {
-    const { videoId } = req.params
-    const currentVideo = videos.filter(video => video.id === JSON.parse(videoId));
-    if (currentVideo.length) {
-        res.send(currentVideo);
-    } else {
-        res.status(404);
-        res.send("This video is not exit")
-    }
-})
+app.get("/products", (req: Request, res: Response) => {
+  res.send(products);
+});
 
+// app.get('/videos', (req: any, res: any) => {
+//     res.status(200);
+//     res.send(videos)
+// })
 
+// app.get('/videos/:videoId', (req: any, res: any) => {
+//     const { videoId } = req.params
+//     const currentVideo = videos.filter(video => video.id === JSON.parse(videoId));
+//     if (currentVideo.length) {
+//         res.send(currentVideo);
+//     } else {
+//         res.status(404);
+//         res.send("This video is not exit")
+//     }
+// })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
